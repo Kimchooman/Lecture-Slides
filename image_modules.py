20c2a349e6702c0a4ac1be83f97d986c1d46ee0a
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2021-07-27 14:09:02 -0400

    Update image_modules.py
    first working version

--- image_modules.py
+++ image_modules.py
@@ -3,10 +3,12 @@ import numpy as np
 
 class ImageChanges:
 
-	NODE_DISTANCE = 10
-	NODE_CHANGE_THRESH = 1
+	CHUNK_PADDING = 20
+	CHUNCK_MARGIN = 10
+	CHUNK_SIZE = 5
+	CHUNK_CHANGE_THRESH = 1
 
-	BLACKWHITE_DELTA_TRESH = 100
+	BLACKWHITE_DELTA_TRESH = 200
 
 	def __init__(self, frame):
 
@@ -16,6 +18,8 @@ class ImageChanges:
 
 		self.prev_bw_frame = None
 
+		self.frame_skip_count = 0
+		
 	def full_frame_bw_comparison(self):
 
 		thresh_exceeded = False
@@ -24,91 +28,51 @@ class ImageChanges:
 		#blurred = cv2.GaussianBlur(gray, (7,7), 0)
 		T, threshInv = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
 
-		if  self.prev_bw_frame != None:
+		if  type(self.prev_bw_frame) != type(None):
 
 			change_count = 0
 
 			for r in range(len(self.prev_bw_frame)):
-				for p in range(self.prev_bw_frame[r]):
+				for p in range(len(self.prev_bw_frame[r])):
 
 					if self.prev_bw_frame[r][p] != threshInv[r][p]:
 						change_count += 1
 
-			if change_count >= BLACKWHITE_DELTA_TRESH:
+			if change_count >= ImageChanges.BLACKWHITE_DELTA_TRESH:
 				thresh_exceeded = True
+
+			self.prev_bw_frame = threshInv
+
+			cv2.imshow("threshInv", threshInv)
 
 		else:
 			self.prev_bw_frame = threshInv
 
 		return thresh_exceeded
 
-	def node_change_perc(self):
-
+	def chunk_change_perc(self):
 		thresh_exceeded = False
-
-		if self.prev_node_ls != []:
-
-			pixel_delta = (0, 0, 0)
-			count = 0
-			for r in range(len(self.frame)): 
-				for p in range(0, len(self.frame[r]), ImageChanges.NODE_DISTANCE):
-
-					if p != 0:
-
-						prev_r, prev_g, prev_b = self.prev_node_ls[r][p]
-						curr_r, curr_g, curr_b = self.frame[r][p]
-
-						print(prev_r, prev_g, prev_b)
-						print(curr_r, curr_g, curr_b)
-
-						delta_r = abs(prev_r - curr_r)
-						delta_g = abs(prev_g - curr_g)
-						delta_b = abs(prev_b - curr_b)
-
-						r, g, b = pixel_delta
-
-						pixel_delta = (r + delta_r, g + delta_g, b + delta_b)
-
-						self.prev_node_ls[r][p] = self.frame[r][p]
-						count += 1
-
-			r_total, g_total, b_total = pixel_delta
-
-			r = r_total / count
-			g = g_total / count
-			b = b_total / count
-
-			if r >= NODE_CHANGE_THRESH:
-				thresh_exceeded = True
-
-			elif g >= NODE_CHANGE_THRESH:
-				thresh_exceeded = True
-
-			elif b >= NODE_CHANGE_THRESH:
-				thresh_exceeded = True
-
-		else:
-			count = 0
-			for r in range(len(self.frame)):
-
-				self.prev_node_ls.append([])
-				for p in range(0, len(self.frame[r])):
-
-
-					if count == ImageChanges.NODE_DISTANCE:
-						self.prev_node_ls[r].append(self.frame[r][p])
-						count = 0
-
-					else:
-						count += 1
-						self.prev_node_ls[r].append(False)
-
 		return thresh_exceeded
 
-	def eval_change(self, NODES=False, BWthresh=False):
+	def view_new_image(self):
+		return self.frame
 
-		if NODES == True:
-			node_bool = self.node_change_perc()
+	def eval_change(self, frame_skip=10,NODES=False, BWthresh=False):
 
-		if BWthresh == True:
-			bw_comp_bool = self.full_frame_bw_comparison()
+		bw_comp_bool = None
+		node_bool = None
+
+		if self.frame_skip_count >= frame_skip:
+
+			if NODES == True:
+				node_bool = self.chunk_change_perc()
+
+			if BWthresh == True:
+				bw_comp_bool = self.full_frame_bw_comparison()
+
+			self.frame_skip_count = 0
+
+		else:
+			self.frame_skip_count += 1
+
+		return bw_comp_bool
